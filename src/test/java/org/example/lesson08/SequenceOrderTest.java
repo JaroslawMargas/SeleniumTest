e8b70964be20e7bba15841b182bb3aa24dfbf79d
BABA <jaromarg@gmail.com>
2020-03-22 16:52:56 +0100

    Test for Scanner input and System.out

--- src/test/java/org/example/lesson08/SequenceOrderTest.java
+++ src/test/java/org/example/lesson08/SequenceOrderTest.java
@@ -1,11 +1,45 @@
 package org.example.lesson08;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SequenceOrderTest {
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    private void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
 
     @Test
     void checkSeq0() {
@@ -65,5 +99,17 @@ class SequenceOrderTest {
     @Test
     void checkSeq11() {
         assertTrue(SequenceOrder.checkSeq("0"));
+    }
+    @Test
+    /*
+    // How to set up Input as Scanner.next()
+    // and get expected output as System.out.print()
+     */
+    public void checkSeqScanner1() {
+        final String testString = "1\n2\n0\n";
+        provideInput(testString);
+        setUpOutput();
+        SequenceOrder.checkSeqScanner();
+        assertEquals("true", getOutput());
     }
 }
