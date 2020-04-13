77c2406e592663c2a54488afd94c74f327e4be6f
BABA <jaromarg@gmail.com>
2020-04-13 18:15:36 +0200

    change location

--- src/main/java/org/example/lesson05/RationalOperator.java
+++
@@ -1,7 +0,0 @@
-package org.example.lesson05;
-
-public class RationalOperator {
-    public boolean checkNumberInRange(int number, int moreThen, int lessThen) {
-        return number < lessThen && number > moreThen;
-    }
-}
